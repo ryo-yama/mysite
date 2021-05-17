0189b6d11c995f4da6c4b3fb4769eb4e8827fa96
ryo-yama <johnfun.4649.aa@gmail.com>
2021-05-17 16:08:27 +0900

    new file:   numpy_tutorial.py

---
+++ numpy_tutorial.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+x = np.array([1.0, 2.0, 3.0])
+y = np.array([6.0, 5.0, 4.0])
+
+print(x)
+print(type(x))
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+
+x = np.array([[1.0, 2.0], [3.0, 4.0]])
+print(x)
+print(x.shape)
+print(x.dtype)
